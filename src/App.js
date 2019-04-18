b8e3ad3eb3384bbb1f669f30cdf6373799a6b76a
Vince <vince@nasthon.com>
2019-04-18 15:57:49 +0000

    test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload. Testing.
+            Edit <code>src/App.js</code> and save to reload. 測試
           </p>
           <a
             className="App-link"
